c1f9ec7c9e8da72e0044205bdf0b877dc91f1569
Prakhar Sankrityayan <prakhar3099@gmail.com>
2022-08-09 23:26:51 +0530

    login with BE

--- react-app/src/screens/Login.jsx
+++ react-app/src/screens/Login.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { hostNameUrl } from "../config/api";
 import "../styles/login.css";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function Login() {
   const navigate = useNavigate();
@@ -8,6 +16,14 @@ function Login() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   // User Login info
   const database = [
@@ -40,25 +56,27 @@ function Login() {
     //Prevent page reload
     event.preventDefault();
 
-    var { uname, pass } = document.forms[0];
+    const { uname, pass } = document.forms[0];
 
     // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        localStorage.setItem('username', userData.username);
-        localStorage.setItem('password', userData.password);
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
+    var body = {
+      username: uname.value,
+      password: pass.value
     }
+    axios({
+        method: 'post',
+        url: `${hostNameUrl}/api/token`,
+        data: body
+    })
+    .then(function (response) {
+        console.log(response);
+        localStorage.setItem('username', uname.value);
+        setIsSubmitted(true);
+    })
+    .catch(function (error) {
+        console.log(error);
+        setOpen(true);
+    });
   };
 
   // Generate JSX code for error message
@@ -69,6 +87,11 @@ function Login() {
 
   return (
     <div className="app">
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          Wrong Credentials!
+        </Alert>
+      </Snackbar>
       <div className="login-text">FIC Bond Tracker</div>
       <div className="login-form">
         <div className="title">Sign In</div>
